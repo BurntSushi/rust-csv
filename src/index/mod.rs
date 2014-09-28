a68f3d96d442656ccc8a76a9deb5a51f2455a181
Andrew Gallant <jamslam@gmail.com>
2014-09-28 17:08:45 -0400

    Simplify index sub-module.
    I feel like there's room for more convenience functions, but
    I'm going to let it simmer with a bare bones interface for now.

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -1,7 +1,6 @@
 #![allow(missing_doc)]
 
-use std::io::{mod, File};
-use std::path::BytesContainer;
+use std::io;
 
 use {CsvResult, ErrIo, Reader};
 
@@ -10,18 +9,10 @@ pub struct Indexed<R, I> {
     idx: I,
 }
 
-impl Indexed<File, File> {
-    pub fn from_file(csv_path: &Path) -> CsvResult<Indexed<File, File>> {
-        let rdr = try!(File::open(csv_path).map_err(ErrIo));
-        let idx = try!(File::open(&idx_path(csv_path)).map_err(ErrIo));
-        Ok(Indexed::new(rdr, idx))
-    }
-}
-
 impl<R: io::Reader + io::Seek, I: io::Reader + io::Seek> Indexed<R, I> {
-    pub fn new(rdr: R, idx: I) -> Indexed<R, I> {
+    pub fn new(rdr: Reader<R>, idx: I) -> Indexed<R, I> {
         Indexed {
-            rdr: Reader::from_reader(rdr).no_headers(),
+            rdr: rdr.no_headers(),
             idx: idx,
         }
     }
@@ -30,7 +21,6 @@ impl<R: io::Reader + io::Seek, I: io::Reader + io::Seek> Indexed<R, I> {
         // Why does `seek` want an `i64`?
         try!(self.idx.seek((i * 8) as i64, io::SeekSet).map_err(ErrIo));
         let offset = try!(self.idx.read_be_u64().map_err(ErrIo));
-        println!("OFFSET: {}", offset);
         self.rdr.seek(offset as i64, io::SeekSet)
     }
 
@@ -39,23 +29,12 @@ impl<R: io::Reader + io::Seek, I: io::Reader + io::Seek> Indexed<R, I> {
     }
 }
 
-pub fn create_file(csv_path: &Path) -> CsvResult<()> {
-    create(File::open(csv_path), File::create(&idx_path(csv_path)))
-}
-
 pub fn create<R: io::Reader + io::Seek, W: io::Writer>
-             (csv_rdr: R, mut idx_wtr: W) -> CsvResult<()> {
-    let mut rdr = Reader::from_reader(csv_rdr);
+             (csv_rdr: Reader<R>, mut idx_wtr: W) -> CsvResult<()> {
+    let mut rdr = csv_rdr.no_headers();
     while !rdr.done() {
-        println!("WRITING OFFSET: {}", rdr.byte_offset());
         try!(idx_wtr.write_be_u64(rdr.byte_offset()).map_err(ErrIo));
         for field in rdr { let _ = try!(field); }
     }
     Ok(())
-}
-
-fn idx_path(csv_path: &Path) -> Path {
-    let mut p = csv_path.container_into_owned_bytes();
-    p.push_all(".idx".as_bytes());
-    Path::new(p)
 }
