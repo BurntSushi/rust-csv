97e4a399a49dcb565ebe7199240447428b190aec
Andrew Gallant <jamslam@gmail.com>
2014-10-19 01:09:48 -0400

    Don't use the unsafe iterator in the benchmark.
    (The unsafe iterator is my dirty little secret for now.)

--- bench/huge.rs
+++ bench/huge.rs
@@ -6,6 +6,11 @@ fn main() {
     let huge = "../examples/data/ss10pusa.csv";
     let mut rdr = csv::Reader::from_file(&Path::new(huge));
     while !rdr.done() {
-        for field in rdr { let _ = field.unwrap(); }
+        loop {
+            match rdr.next_field() {
+                None => break,
+                Some(f) => { f.unwrap(); }
+            }
+        }
     }
 }
