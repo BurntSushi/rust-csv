87a983f3f5c9b5eefdf1575f3cce7973bf126bf4
Andrew Gallant <jamslam@gmail.com>
2014-12-22 18:33:01 -0500

    Fix the stream example.

--- examples/stream.rs
+++ examples/stream.rs
@@ -2,28 +2,24 @@ extern crate csv;
 
 use std::comm::channel;
 use std::io;
-use std::task::spawn;
+use std::thread::Thread;
 use std::time::Duration;
 
 fn main() {
     let (send, recv) = channel();
-    spawn(move || {
+    let _ = Thread::spawn(move || {
         let w = io::ChanWriter::new(send);
         let mut enc = csv::Writer::from_writer(w);
         for x in range(1u, 6) {
-            match enc.encode((x, x * x)) {
-                Ok(_) => {},
-                Err(err) => panic!("Failed encoding: {}", err),
+            if let Err(err) = enc.encode((x, x * x)) {
+                panic!("Failed encoding: {}", err);
             }
-            io::timer::sleep(Duration::milliseconds(500));
+            io::timer::sleep(Duration::milliseconds(200));
         }
     });
 
-    let r = io::ChanReader::new(recv);
-    // We create a CSV reader with a small buffer so that we can see streaming
-    // in action on small inputs.
-    let buf = io::BufferedReader::with_capacity(1, r);
-    let mut dec = csv::Reader::from_reader(buf);
+    let mut dec = csv::Reader::from_reader(io::ChanReader::new(recv))
+                              .has_headers(false);
     for r in dec.records() {
         println!("Record: {}", r);
     }
