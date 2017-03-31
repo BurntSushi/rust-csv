b10dab1af5c60086765e93ac8abb8d2cae7cc022
Andrew Gallant <jamslam@gmail.com>
2017-03-31 14:47:45 -0400

    progress

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,30 +7,11 @@ use std::io;
 
 use test::Bencher;
 
-use csv::{ByteRecord, Reader, ReaderBuilder, ReadField};
+use csv::{ByteRecord, Reader, ReaderBuilder};
 
-static NFL: &'static str = include_str!("../examples/data/bench.csv");
+static NFL: &'static str = include_str!("../examples/data/nfl.csv");
 static GAME: &'static str = include_str!("../examples/data/game.csv");
-
-#[bench]
-fn count_nfl_field_bytes(b: &mut Bencher) {
-    let data = NFL.as_bytes();
-    b.bytes = data.len() as u64;
-    b.iter(|| {
-        let mut rdr = ReaderBuilder::new().from_reader(data);
-        assert_eq!(count_read_field_bytes(&mut rdr), 130000);
-    })
-}
-
-#[bench]
-fn count_game_field_bytes(b: &mut Bencher) {
-    let data = GAME.as_bytes();
-    b.bytes = data.len() as u64;
-    b.iter(|| {
-        let mut rdr = ReaderBuilder::new().from_reader(data);
-        assert_eq!(count_read_field_bytes(&mut rdr), 600000);
-    })
-}
+static POP: &'static str = include_str!("../examples/data/worldcitiespop.csv");
 
 #[bench]
 fn count_nfl_record_bytes(b: &mut Bencher) {
@@ -52,16 +33,14 @@ fn count_game_record_bytes(b: &mut Bencher) {
     })
 }
 
-fn count_read_field_bytes<R: io::Read>(rdr: &mut Reader<R>) -> u64 {
-    let mut count = 0;
-    let mut field = Vec::with_capacity(1024);
-    loop {
-        match rdr.read_field_bytes(&mut field).unwrap() {
-            ReadField::Field | ReadField::Record => { count += 1 }
-            ReadField::End => break,
-        }
-    }
-    count
+#[bench]
+fn count_pop_record_bytes(b: &mut Bencher) {
+    let data = POP.as_bytes();
+    b.bytes = data.len() as u64;
+    b.iter(|| {
+        let mut rdr = ReaderBuilder::new().from_reader(data);
+        assert_eq!(count_read_record_bytes(&mut rdr), 20001);
+    })
 }
 
 fn count_read_record_bytes<R: io::Read>(rdr: &mut Reader<R>) -> u64 {
