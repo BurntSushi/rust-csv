85aec19a5efef58d8560f8e51a39e9ad9c5a223e
Andrew Gallant <jamslam@gmail.com>
2017-04-15 09:49:49 -0400

    progress

--- benches/bench.rs
+++ benches/bench.rs
@@ -113,36 +113,36 @@ bench_serde!(
     count_nfl_deserialize_bytes, NFL, count_deserialize_bytes, NFLRow, 9999);
 bench_serde!(
     count_nfl_deserialize_str, NFL, count_deserialize_str, NFLRow, 9999);
-bench!(count_nfl_iter_bytes, NFL, count_iter_bytes, 10000);
-bench!(count_nfl_iter_str, NFL, count_iter_str, 10000);
-bench!(count_nfl_read_bytes, NFL, count_read_bytes, 10000);
-bench!(count_nfl_read_str, NFL, count_read_str, 10000);
+bench!(count_nfl_iter_bytes, NFL, count_iter_bytes, 1234606);
+bench!(count_nfl_iter_str, NFL, count_iter_str, 1234606);
+bench!(count_nfl_read_bytes, NFL, count_read_bytes, 1234606);
+bench!(count_nfl_read_str, NFL, count_read_str, 1234606);
 bench_serde!(
     no_headers,
     count_game_deserialize_bytes, GAME, count_deserialize_bytes, GAMERow, 100000);
 bench_serde!(
     no_headers,
     count_game_deserialize_str, GAME, count_deserialize_str, GAMERow, 100000);
-bench!(count_game_iter_bytes, GAME, count_iter_bytes, 100000);
-bench!(count_game_iter_str, GAME, count_iter_str, 100000);
-bench!(count_game_read_bytes, GAME, count_read_bytes, 100000);
-bench!(count_game_read_str, GAME, count_read_str, 100000);
+bench!(count_game_iter_bytes, GAME, count_iter_bytes, 1400000);
+bench!(count_game_iter_str, GAME, count_iter_str, 1400000);
+bench!(count_game_read_bytes, GAME, count_read_bytes, 1400000);
+bench!(count_game_read_str, GAME, count_read_str, 1400000);
 bench_serde!(
     count_pop_deserialize_bytes, POP, count_deserialize_bytes, POPRow, 20000);
 bench_serde!(
     count_pop_deserialize_str, POP, count_deserialize_str, POPRow, 20000);
-bench!(count_pop_iter_bytes, POP, count_iter_bytes, 20001);
-bench!(count_pop_iter_str, POP, count_iter_str, 20001);
-bench!(count_pop_read_bytes, POP, count_read_bytes, 20001);
-bench!(count_pop_read_str, POP, count_read_str, 20001);
+bench!(count_pop_iter_bytes, POP, count_iter_bytes, 815677);
+bench!(count_pop_iter_str, POP, count_iter_str, 815677);
+bench!(count_pop_read_bytes, POP, count_read_bytes, 815677);
+bench!(count_pop_read_str, POP, count_read_str, 815677);
 bench_serde!(
     count_mbta_deserialize_bytes, MBTA, count_deserialize_bytes, MBTARow, 9999);
 bench_serde!(
     count_mbta_deserialize_str, MBTA, count_deserialize_str, MBTARow, 9999);
-bench!(count_mbta_iter_bytes, MBTA, count_iter_bytes, 10000);
-bench!(count_mbta_iter_str, MBTA, count_iter_str, 10000);
-bench!(count_mbta_read_bytes, MBTA, count_read_bytes, 10000);
-bench!(count_mbta_read_str, MBTA, count_read_str, 10000);
+bench!(count_mbta_iter_bytes, MBTA, count_iter_bytes, 533496);
+bench!(count_mbta_iter_str, MBTA, count_iter_str, 533496);
+bench!(count_mbta_read_bytes, MBTA, count_read_bytes, 533496);
+bench!(count_mbta_read_str, MBTA, count_read_str, 533496);
 
 fn count_deserialize_bytes<R, D>(rdr: &mut Reader<R>) -> u64
     where R: io::Read, D: Deserialize
@@ -170,8 +170,7 @@ fn count_deserialize_str<R, D>(rdr: &mut Reader<R>) -> u64
 fn count_iter_bytes<R: io::Read>(rdr: &mut Reader<R>) -> u64 {
     let mut count = 0;
     for rec in rdr.byte_records() {
-        let _ = rec.unwrap();
-        count += 1;
+        count += rec.unwrap().as_slice().len() as u64;
     }
     count
 }
@@ -179,8 +178,7 @@ fn count_iter_bytes<R: io::Read>(rdr: &mut Reader<R>) -> u64 {
 fn count_iter_str<R: io::Read>(rdr: &mut Reader<R>) -> u64 {
     let mut count = 0;
     for rec in rdr.records() {
-        let _ = rec.unwrap();
-        count += 1;
+        count += rec.unwrap().as_slice().len() as u64;
     }
     count
 }
@@ -189,7 +187,7 @@ fn count_read_bytes<R: io::Read>(rdr: &mut Reader<R>) -> u64 {
     let mut count = 0;
     let mut rec = ByteRecord::new();
     while !rdr.read_byte_record(&mut rec).unwrap() {
-        count += 1;
+        count += rec.as_slice().len() as u64;
     }
     count
 }
@@ -198,7 +196,7 @@ fn count_read_str<R: io::Read>(rdr: &mut Reader<R>) -> u64 {
     let mut count = 0;
     let mut rec = StringRecord::new();
     while !rdr.read_record(&mut rec).unwrap() {
-        count += 1;
+        count += rec.as_slice().len() as u64;
     }
     count
 }
