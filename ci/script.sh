bb6a0768409f6ba476cacfbdd854422b2ccb192b
Andrew Gallant <jamslam@gmail.com>
2018-01-01 10:05:13 -0500

    ci+deps: use workspace and reorganize CI

---
+++ ci/script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+ci/check-copy cookbook
+ci/check-copy tutorial
+cargo build --verbose
+cargo doc --verbose
+cargo test --verbose --manifest-path csv-core/Cargo.toml
+cargo test --verbose --manifest-path csv-index/Cargo.toml
+if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
+  cargo bench --verbose --no-run
+fi
