e84004c3e96391cb604c1bbc72e04534ee5db34a
Andrew Gallant <jamslam@gmail.com>
2018-07-18 19:45:45 -0400

    ci: test csv crate proper
    I can't believe this wasn't already in CI. Weird.

--- ci/script.sh
+++ ci/script.sh
@@ -6,6 +6,7 @@ ci/check-copy cookbook
 ci/check-copy tutorial
 cargo build --verbose
 cargo doc --verbose
+cargo test --verbose
 cargo test --verbose --manifest-path csv-core/Cargo.toml
 cargo test --verbose --manifest-path csv-index/Cargo.toml
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
