da50425d5b589ff64fbd29e5b5bd59750f5c703c
Andrew Gallant <jamslam@gmail.com>
2019-07-30 19:45:41 -0400

    ci: switch to stable rustfmt
    This is generally more convenient for contributors and also generally
    makes more sense.

--- ci/script.sh
+++ ci/script.sh
@@ -7,10 +7,11 @@ cargo doc --verbose
 cargo test --verbose
 cargo test --verbose --manifest-path csv-core/Cargo.toml
 cargo test --verbose --manifest-path csv-index/Cargo.toml
-if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-  rustup component add rustfmt --toolchain nightly-x86_64-unknown-linux-gnu
-
+if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
+  rustup component add rustfmt
   cargo fmt -- --check
+fi
+if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
   ci/check-copy cookbook
   ci/check-copy tutorial
   cargo bench --verbose --no-run
