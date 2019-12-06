9f5455bc6a1ffcbfd7ac993dd87e023dd29cb415
Robert-André Mauchin <30413512+eclipseo@users.noreply.github.com>
2019-12-06 17:53:47 +0100

    deps: bump arrayvec to 0.5
    And also, don't run tests in CI on our MSRV since arrayvec has a higher
    MSRV.
    
    Closes #182

--- ci/script.sh
+++ ci/script.sh
@@ -4,6 +4,13 @@ set -ex
 
 cargo build --verbose
 cargo doc --verbose
+
+# Our dev dependencies want newer versions of Rust. Instead of bumping our
+# MSRV, we just don't test on our MSRV.
+if [ "$TRAVIS_RUST_VERSION" = "1.33.0" ]; then
+  exit 0
+fi
+
 cargo test --verbose
 cargo test --verbose --manifest-path csv-core/Cargo.toml
 cargo test --verbose --manifest-path csv-index/Cargo.toml
